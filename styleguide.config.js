04ef32edc6e4ecfde6b65275ae65e5f60461a77e
David Kleiman <dave.kleiman2@gmail.com>
2019-06-06 18:49:05 -0400

    Add styleguide config

---
+++ styleguide.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: 'KYN Design System',
+  sections: [
+    {
+      name: 'Documentation',
+      sections: [
+        {
+          name: 'Overview',
+          content: 'docs/overview.md'
+        },
+        {
+          name: 'Useful Commands',
+          content: 'docs/commands.md'
+        },
+        {
+          name: 'Contributing',
+          content: 'docs/contributing.md'
+        },
+      ]
+    },
+    {
+      name: 'Components',
+      components: 'src/lib/**/[A-Z]*.{js,jsx,ts,tsx}',
+      ignore: [
+        'src/lib/utils/**',
+      ],
+    },
+  ],
+};
