8a64028e794d7c5b498d5402bce531a73f2c75ec
David Kleiman <dave.kleiman2@gmail.com>
2019-06-08 23:29:36 -0400

    Update styleguide

---
+++ precache-manifest.49eea8bd9a4e7de9d202a3dfad966ada.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b82fb70b206ee4272edaa22d9ce0581",
+    "url": ".DS_Store"
+  },
+  {
+    "revision": "14c837243b907ff5d4ba",
+    "url": "build/2.14c83724.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "7275648d3befe6342f9d",
+    "url": "build/main.7275648d.js"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "fonts/.DS_Store"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "ed6a98d002bc0b535dd8618f3ae05fe7",
+    "url": "fonts/MaterialIcons-Regular.ijmap"
+  },
+  {
+    "revision": "a1adea65594c502f9d9428f13ae210e1",
+    "url": "fonts/MaterialIcons-Regular.svg"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "f865a6e13c40dd82161655d68b325d08",
+    "url": "fonts/material-icons.css"
+  },
+  {
+    "revision": "183ebd8abb29e67745a987e8a2e5a507",
+    "url": "index.html"
+  },
+  {
+    "revision": "7275648d3befe6342f9d",
+    "url": "static/css/main.e3bc1332.chunk.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
