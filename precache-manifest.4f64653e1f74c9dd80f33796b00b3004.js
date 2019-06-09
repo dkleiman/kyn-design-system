3f129341fdb9804b45f448366f36b6f8ab124a4e
David Kleiman <dave.kleiman2@gmail.com>
2019-06-08 21:53:51 -0400

    Update style guide

---
+++ precache-manifest.4f64653e1f74c9dd80f33796b00b3004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf9fea19432e0314579",
+    "url": "build/2.0bf9fea1.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "094b81a22d41a11544a0",
+    "url": "build/main.094b81a2.js"
+  },
+  {
+    "revision": "941427a104745384960e686ca9d0ece5",
+    "url": "index.html"
+  },
+  {
+    "revision": "094b81a22d41a11544a0",
+    "url": "static/css/main.1e95cd6c.chunk.css"
+  }
+]);
